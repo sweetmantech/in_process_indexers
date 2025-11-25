83bdab847b8326e3b7fc107348cc8e382de70396
kcethdev <tech.eng.me@gmail.com>
2025-11-24 23:30:33 -0500

    fix id

--- src/handlers/In_Process_Moment_Admins.ts
+++ src/handlers/In_Process_Moment_Admins.ts
@@ -3,7 +3,7 @@ import { InProcessMoment, InProcess_Moment_Admins } from "generated";
 InProcessMoment.UpdatedPermissions.handler(
   async ({ event, context }) => {
     const entity: InProcess_Moment_Admins = {
-      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+      id: `${event.srcAddress.toLowerCase()}_${Number(event.params.tokenId)}_${event.chainId}_${event.block.number}_${event.logIndex}`,
       collection: event.srcAddress.toLowerCase(),
       token_id: Number(event.params.tokenId),
       admin: event.params.user.toLowerCase(),
--- src/handlers/In_Process_Moment_Comments.ts
+++ src/handlers/In_Process_Moment_Comments.ts
@@ -2,8 +2,8 @@ import { InProcessERC20Minter, InProcess_Moment_Comments } from "generated";
 
 InProcessERC20Minter.MintComment.handler(async ({ event, context }) => {
   const entity: InProcess_Moment_Comments = {
-    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    sender: event.params.sender,
+    id: `${event.params.tokenContract.toLowerCase()}_${Number(event.params.tokenId)}_${event.chainId}_${event.block.number}_${event.logIndex}`,
+    sender: event.params.sender.toLowerCase(),
     collection: event.params.tokenContract.toLowerCase(),
     token_id: Number(event.params.tokenId),
     comment: event.params.comment,
