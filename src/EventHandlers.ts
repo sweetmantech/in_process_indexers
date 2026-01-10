e8f5d81e659cae8bd1d55c5baa683ed5f4b546ed
k.c <tech.eng.me@gmail.com>
2026-01-10 15:18:31 +1000

    trigger the new build

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,8 +1,3 @@
-/*
- * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
- * This file imports all event handlers organized by contract
- */
-
 // Import all handlers - they register themselves when imported
 import "./handlers/In_Process_Collections";
 import "./handlers/In_Process_Admins";
